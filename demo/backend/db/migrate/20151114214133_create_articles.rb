815a4814c9b0aeef6b813ae189085e3a2b618cd5
Benjamin Borowski <ben.borowski@typeoneerror.com>
2015-11-14 13:47:51 -0800

    Adds Article model

---
+++ demo/backend/db/migrate/20151114214133_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.references :author, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
